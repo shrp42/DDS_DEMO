6cf71dd55ef07a27f2e33dd37f976033cacb55e3
SHRP12 <architect117711@gmail.com>
2026-02-14 16:18:19 +0300

    Добавил похожие товары и активную ссылку на товар с ценой/описанием

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,5 +8,6 @@ from core.views import index, contact
 urlpatterns = [
     path('admin/', admin.site.urls),  # админка
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
