8f1697902062d580e13e9854b79a32a107f9660d
SHRP12 <architect117711@gmail.com>
2026-02-15 16:49:24 +0300

    Добавлена аунтефикация

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index, contact
-
 urlpatterns = [
     path('admin/', admin.site.urls),  # админка
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ core/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Имя Пользователя',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Имя Пользователя',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Введите email',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Введите пароль',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Повторите пароль',
+        'class': 'w-full py-4 px-6 rounded-xl border focus:outline-none focus:ring-2 focus:ring-teal-700 transition'
+    }))
+
